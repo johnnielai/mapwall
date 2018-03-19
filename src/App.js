f82570b5907e028595eb55d6608baa63b406550f
JL <johnnie-l@Live.ca>
2018-03-18 23:31:00 -0700

    Changing image location

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import TopNav from './TopNav/topnav.js';
 
+import phone from './images/Phone.png';
 import './App.css';
 
 
@@ -13,7 +14,7 @@ class App extends Component {
         <div className="content">
           <div className="phone-select"></div>
           <div className="phone-overlay">
-            <img src='../Phone.png'/>
+            <img src={phone}/>
           </div>
         </div>
         <Particles 
