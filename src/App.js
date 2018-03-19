7cf394af4d401803d43c64e269a930d64eb55953
JL <johnnie-l@Live.ca>
2018-03-18 23:22:28 -0700

    General layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Particles from 'react-particles-js';
+import TopNav from './TopNav/topnav.js';
+
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="main">
+        <TopNav></TopNav>
+        <div className="content">
+          <div className="phone-select"></div>
+          <div className="phone-overlay">
+            <img src='../Phone.png'/>
+          </div>
+        </div>
+        <Particles 
+          params={{
+            particles: {
+              line_linked: {
+                shadow: {
+                  enable: true,
+                  color: "#ffffff",
+                  blur: 1
+                }
+              }
+            }
+          }}
+          style={{
+            position: 'fixed',
+            top: '0px',
+            left: '0px',
+            width: '100%',
+            height: '100%',
+          }}/>
       </div>
     );
   }
---
+++ src/TopNav/topnav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './topnav.css';
+
+class TopNav extends Component {
+  render() {
+    return (
+      <nav className="top-nav-container">
+        <div className="navigation">
+            <div className="nav-title">MapWall</div>
+                
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default TopNav;
